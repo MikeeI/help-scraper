ea164997aee74079175748fa1fd6884da70cd1ef
CaptainPlan <99226632+CaptainPlan@users.noreply.github.com>
2022-02-21 04:13:59 +0100

    Update zstd.sh

--- zstd.sh
+++ zstd.sh
@@ -6,7 +6,7 @@ wget https://github.com/facebook/zstd/releases/download/v1.4.5/zstd-1.4.5.tar.gz
 sudo tar xvf zstd-1.4.5.tar.gz
 cd zstd-1.4.5
 sudo make install
-cd ~/
+cd ..
 ls -lah
 sudo ./zstd-1.4.5/programs/zstd --help > zstd/zstd-help.txt
 sudo ./zstd-1.4.5/programs/zstd --version > zstd/zstd-version.txt
