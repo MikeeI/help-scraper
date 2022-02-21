21d57cad2c28404bcfc887f3bf4900695baecfa7
CaptainPlan <99226632+CaptainPlan@users.noreply.github.com>
2022-02-21 03:44:12 +0100

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#sudo apt update
-#sudo apt install -y ffmpeg
+sudo apt update
+sudo apt install -y ffmpeg
 mkdir -p ffmpeg
-wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
-sudo tar -xvf ffmpeg-release-amd64-static.tar.xz
-./ffmpeg-5.0-amd64-static/ffmpeg --help > ffmpeg/ffmpeg-help.txt
-./ffmpeg-5.0-amd64-static/ffmpeg --version > ffmpeg/ffmpeg-version.txt
-sudo rm -rf ffmpeg-release-amd64-static.tar.xz ffmpeg-5.0-amd64-static
+#wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+#sudo tar -xvf ffmpeg-release-amd64-static.tar.xz
+#./ffmpeg-5.0-amd64-static/ffmpeg --help > ffmpeg/ffmpeg-help.txt
+#./ffmpeg-5.0-amd64-static/ffmpeg -version > ffmpeg/ffmpeg-version.txt
+#sudo rm -rf ffmpeg-release-amd64-static.tar.xz ffmpeg-5.0-amd64-static
+ffmpeg --help > ffmpeg/ffmpeg-help.txt
+ffmpeg -version > ffmpeg/ffmpeg-version.txt
