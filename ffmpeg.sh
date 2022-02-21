ec7364db09c54420b91e4c8e941a143264ea6423
CaptainPlan <99226632+CaptainPlan@users.noreply.github.com>
2022-02-21 03:36:56 +0100

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -6,4 +6,4 @@ wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.x
 sudo tar -xvf ffmpeg-release-amd64-static.tar.xz
 ./ffmpeg-5.0-amd64-static/ffmpeg --help > ffmpeg/ffmpeg-help.txt
 ./ffmpeg-5.0-amd64-static/ffmpeg --version > ffmpeg/ffmpeg-version.txt
-rm -rf ffmpeg-release-amd64-static.tar.xz ffmpeg-5.0-amd64-static
+sudo rm -rf ffmpeg-release-amd64-static.tar.xz ffmpeg-5.0-amd64-static
